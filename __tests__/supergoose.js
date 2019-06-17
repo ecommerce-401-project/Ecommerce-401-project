0fb3efe75ec40fc6c5283e9634b04e7dbc2decf2
Michele Saba <michelemsaba@gmail.com>
2019-06-17 14:56:48 -0500

    created gameRepo and supergoose file

---
+++ __tests__/supergoose.js
@@ -0,0 +1,49 @@
+/**
+ * Combines SuperTest and Mongoose Memory Server
+ * to reduce (hopefully) the pain of
+ * testing a Mongoose API
+ */
+
+const mongoose = require('mongoose');
+const MongoMemoryServer = require('mongodb-memory-server').default;
+const supertest = require('supertest');
+
+let mongoServer;
+
+let supergoose = (module.exports = {});
+/**
+ * @server
+ * @returns function that expects an express server
+ */
+supergoose.server = server => supertest(server);
+
+/**
+ * Typically used in Jest beforeAll hook
+ */
+supergoose.startDB = async () => {
+  mongoServer = new MongoMemoryServer();
+
+  const mongoUri = await mongoServer.getConnectionString();
+
+  const mongooseOptions = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  };
+
+  await mongoose.connect(mongoUri, mongooseOptions);
+};
+
+/**
+ * Typically used in Jest afterAll hook
+ */
+supergoose.stopDB = async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+};
+
+// Just so that it can live in the tests folder
+describe('supergoose', () => {
+  it('is super', () => {
+    expect(true).toBeTruthy();
+  });
+});
--- src/models/games-model.js
+++ src/models/games-model.js
@@ -1,2 +1,26 @@
-'use stict';
+'use strict';
 
+const Game = require('./games-schema');
+
+class GameRepository {
+  getAll() {
+    return Promise.resolve(Game.find());
+  }
+  async getById(_id) {
+    return await Game.find(_id);
+  }
+  create(game) {
+    let newGame = new Game(game); 
+    return newGame.save();
+  } 
+  async update(_id, entry) {
+    let gameToUpdate = await Game.findOne({ _id });
+    Object.assign(gameToUpdate, entry);
+    return await gameToUpdate.save();
+  }
+  delete(_id) {
+    return Game.deleteOne({ _id });
+  }
+}
+
+module.exports = GameRepository;
