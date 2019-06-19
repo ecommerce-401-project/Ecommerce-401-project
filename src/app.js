6826f3355c2d03d865f4f14ef35651a0b5ebb1b0
Michele Saba <michelemsaba@gmail.com>
2019-06-19 15:19:59 -0500

    Wire up 404/500 middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const errorHandler = require( './middleware/error.js');
+const notFound = require( './middleware/404.js' );
 
 const app = express();
 app.use(cors());
 app.use(morgan('dev'));
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +16,12 @@ app.use(require('./routes/player-routes'));
 app.use(require('./routes/publisher-routes'));
 app.use(require('./routes/admin-routes'));
 app.use(require('./auth/routes/auth-router'));
+
+
+app.use(notFound);
+app.use(errorHandler);
+
+
 module.exports = {
   server: app,
   start: port =>
