1ca7a89c4218e3ae6ad660b4bfc11aba69fd6d9f
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-06-20 09:29:26 -0500

    added swagger route '/'

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ const cors = require('cors');
 const morgan = require('morgan');
 const errorHandler = require( './middleware/error.js');
 const notFound = require( './middleware/404.js' );
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 
-
+app.use('/', swaggerUi.serve);
+app.get('/', swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
