d04de9a2778c525d5f99a63d9c240c4e29991403
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-06-20 17:33:13 -0500

    added swagger packages with inital setup

--- src/app.js
+++ src/app.js
@@ -3,12 +3,32 @@ const cors = require('cors');
 const morgan = require('morgan');
 const errorHandler = require( './middleware/error.js');
 const notFound = require( './middleware/404.js' );
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 
 const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 
+const options = {
+  definition: {
+    openapi: '3.0.0', // Specification (optional, defaults to swagger: '2.0')
+    info: {
+      title: 'Ecommerce 401 Project', // Title (required)
+      version: '1.0.0', // Version (required)
+    },
+  },
+  // Path to the API docs
+  apis: [`${__dirname}/routes/*.js`],
+};
 
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+app.get('/api/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,3 +47,8 @@ module.exports = {
   start: port =>
     app.listen(port, () => console.log(`Server up on port ${port}`)),
 };
+
+
+
+
+
