215fe63d0c717a8e68c851e02fa40fdfdffe3ea3
Michele Saba <michelemsaba@gmail.com>
2019-07-26 08:46:03 -0500

    configuring cors

--- src/app.js
+++ src/app.js
@@ -7,10 +7,11 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJSDoc = require('swagger-jsdoc');
 
 const app = express();
-app.use(cors({
-  origin: 'https://localhost:3000',
-  exposedHeaders: ['Content-Length', 'Authorization'],
-}));
+app.use(
+  cors({
+    exposedHeaders: ['Content-Length', 'Authorization'],
+  })
+);
 app.use(morgan('dev'));
 
 const options = {
@@ -57,10 +58,6 @@ app.get('/api/swagger.json', (req, res) => {
 });
 app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get('/', (req, res) => res.redirect('/api'));
-
-
-
-
 
 app.use(notFound);
 app.use(errorHandler);
